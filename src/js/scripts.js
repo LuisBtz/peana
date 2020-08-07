06399c0e0c72f820a9ea3fa0c36a392788c8a6f0
LuisBtz <luisbttf@gmail.com>
2020-08-07 18:50:45 -0500

    home, footer, index, responsive

---
+++ src/js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.ham').on('click', function() {
+        $('nav').toggleClass('clicked');
+    });
+
+
+
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    items:1,
+    autoplay:true,
+    animateOut: 'fadeOut',
+    dots: false
+})
+});

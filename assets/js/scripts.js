dd8255000765eee91947085d25e2a0544d2d16cc
LuisBtz <luisbttf@gmail.com>
2020-08-10 19:29:27 -0500

    single exhibition

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,26 +3,37 @@ $(document).ready(function () {
         $('nav').toggleClass('clicked');
     });
 
-    $('.owl-carousel').owlCarousel({
-    loop:true,
-    nav:false,
-    items:1,
-    autoplay:true,
-    animateOut: 'fadeOut',
-    dots: false,
-    autoplayTimeout: 2500,
-    mouseDrag: false,
-    touchDrag: false
-});
+    $('#home .owl-carousel').owlCarousel({
+        loop:true,
+        nav:false,
+        items:1,
+        autoplay:true,
+        animateOut: 'fadeOut',
+        dots: false,
+        autoplayTimeout: 2500,
+        mouseDrag: false,
+        touchDrag: false
+    });
+
+    $('.slideex.owl-carousel.owl-theme').owlCarousel({
+        loop:true,
+        nav:false,
+        items:3,
+        margin:20,
+        autoWidth:true,
+        autoplay:true,
+        dots: false,
+        autoplayTimeout: 2500
+    });
 
 
 
 var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
  $('ul a').each(function() {
   if (this.href === path) {
-   $(this).addClass('active');
+   $(this).addClass('active-link');
   }
  });
 
- 
+
 });

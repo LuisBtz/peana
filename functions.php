c3682e5a3a6b2f783426e76b4404ddb5a63a3846
LuisBtz <luisbttf@gmail.com>
2020-08-17 19:17:13 -0500

    wordpress config

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+
+// Setup
+
+
+// if( function_exists('acf_add_options_page') ) {
+
+//     acf_add_options_page(array(
+//         'page_title'     => 'Theme General Settings',
+//         'menu_title'    => 'General',
+//         'menu_slug'     => 'theme-general-settings',
+//         'capability'    => 'edit_posts',
+//         'redirect'        => false,
+//         'icon_url'        => 'dashicons-admin-multisite',
+//         'position'         => 7
+//     ));
+
+
+// }
+
+
+// Includes
+
+include( get_template_directory() . '/includes/front/enqueue.php' );
+include( get_template_directory() . '/includes/setup.php' );
+
+
+// Hooks
+
+add_action( 'wp_enqueue_scripts', 'pl_enqueue' );
+add_action( 'after_setup_theme', 'pl_setup_theme' );
+
---
+++ includes/front/enqueue.php
@@ -0,0 +1,22 @@
+<?php
+
+function pl_enqueue(){
+    
+    wp_register_style( 'pl_style', get_template_directory_uri() . '/assets/assets/css/styles.min.css' );
+
+    wp_enqueue_style( 'pl_style' );
+
+    
+
+
+    wp_register_script( 'pl_jquery', get_template_directory_uri() . '/assets/assets/js/jquery.min.js', array(), false, true );
+    wp_register_script( 'pl_owl', get_template_directory_uri() . '/assets/assets/js/owl.carousel.min.js', array(), false, true );
+    wp_register_script( 'pl_action', get_template_directory_uri() . '/assets/js/script.js', array(), false, true );
+  
+
+
+    wp_enqueue_script( 'pl_jquery' );
+    wp_enqueue_script( 'pl_owl' );
+    wp_enqueue_script( 'pl_action' );
+
+}

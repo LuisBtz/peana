75c844fc7904116df84184d5e945c6c671e703eb
LuisBtz <luisbttf@gmail.com>
2020-08-21 18:43:20 -0500

    exhibitions v1

---
+++ page-exhibitions.php
@@ -0,0 +1,28 @@
+<?php
+
+get_header();
+
+//Template Name: Template Exhibitions
+?>
+<div class="container m-top" id="exhibitions">
+    <div class="nav-cat">
+        <ul>
+        <li><a class="active-link" href="current.html">Current</a></li>
+        <li><a href="upcoming.html">Upcoming</a></li>
+        <li><a href="archive.html">Archive</a></li>
+        </ul>
+    </div>
+    <section class="current-grid ajustado">
+        <article><a href="exhibition.html"><img src="assets/images/about-corta.jpg" alt="Peana">
+            <div class="info-ex">
+            <h3>Usando este cuerpo, <br>pensando en la fuente</h3><b>Tomás Díaz Cedeño</b>
+            <p>05.27.2020 — 08.15.2020</p><span>Main Gallery</span>
+            </div></a></article>
+        <article><a href="virtual-single.html"><img src="assets/images/bg-slide1.jpg" alt="Peana">
+            <div class="info-ex">
+            <h3>Usando este cuerpo, <br>pensando en la fuente</h3><b>Tomás Díaz Cedeño</b>
+            <p>05.27.2020 — 08.15.2020</p><span>Project Room</span>
+            </div></a></article>
+    </section>
+</div>
+<?php get_footer(); ?>
